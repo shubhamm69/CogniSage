38e247782ac1b4224bbde9a1f57a24de2fc3e59a
kanu123 <kanu0311@gmail.com>
2024-03-05 04:45:21 +0530

    connect to mongoDb

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+// dotenv.config();
+// mongoose.connect(process.env.MONGO)
+mongoose.connect('mongodb+srv://kanu0311:kanu0311@cognisage.2lw0j1q.mongodb.net/user-database?retryWrites=true&w=majority&appName=cogniSage').then(
+    ()=>{
+        console.log("connected to mongodb");
+    }
+).catch((err)=>{
+    console.log("not connected");
+})
 const app = express();
 app.listen(3000,()=>{
     console.log('server listening on port 3000')
