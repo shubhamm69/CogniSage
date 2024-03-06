5b845b6195493c9cdea6e359f0f1cb62c846e685
kanu123 <kanu0311@gmail.com>
2024-03-06 22:00:02 +0530

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 mongoose.connect('mongodb+srv://kanu0311:kanu0311@cognisage.2lw0j1q.mongodb.net/user-database?retryWrites=true&w=majority&appName=cogniSage').then(
     ()=>{
         console.log("connected to mongodb");
@@ -9,8 +10,10 @@ mongoose.connect('mongodb+srv://kanu0311:kanu0311@cognisage.2lw0j1q.mongodb.net/
     console.log("not connected");
 })
 const app = express();
+app.use(express.json());
 app.listen(3000,()=>{
     console.log('server listening on port 3000')
 });
 
 app.use("/api/user",userRoutes);
+app.use("/api/auth",authRoutes);
