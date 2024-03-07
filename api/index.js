787a784ebd8ab92b91afbf2f938910f804f6734d
kanu123 <kanu0311@gmail.com>
2024-03-08 00:20:40 +0530

    completed update user functionality

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 mongoose.connect('mongodb+srv://kanu0311:kanu0311@cognisage.2lw0j1q.mongodb.net/user-database?retryWrites=true&w=majority&appName=cogniSage').then(
     ()=>{
         console.log("connected to mongodb");
@@ -11,6 +12,7 @@ mongoose.connect('mongodb+srv://kanu0311:kanu0311@cognisage.2lw0j1q.mongodb.net/
 })
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.listen(3000,()=>{
     console.log('server listening on port 3000')
 });
--- client/src/components/Oauth.jsx
+++ client/src/components/Oauth.jsx
@@ -1,34 +1,44 @@
-import {GoogleAuthProvider, signInWithPopup, getAuth} from "firebase/auth";
-import { app } from "../firebase.js";
+import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth';
+import { app } from '../firebase';
 import { useDispatch } from 'react-redux';
-import { signInSuccess } from "../redux/user/userSlice.js";
-export default function Oauth() {
-    const dispatch = useDispatch();
-    const handleGoogleClick = async ()=>{
-        try{
-            const provider = new GoogleAuthProvider();
-            const auth = getAuth(app);
+import { signInSuccess } from '../redux/user/userSlice';
+import { useNavigate } from 'react-router-dom';
 
-            const result = await signInWithPopup(auth, provider);
-            const res = await fetch('/api/auth/google',{
-                method : 'POST',
-                headers:{
-                    'Content-Type':'application/json',
-                },
-                body: JSON.stringify({
-                    name: result.user.displayName,
-                    email: result.user.email,
-                    photo: result.user.photoURL,
-                })
-            })
-            const data = await res.json();
-            dispatch(signInSuccess(data));
-        }
-        catch(error){
-            console.log('could not llogin with google',error);
-        }
+export default function OAuth() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleGoogleClick = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth(app);
+
+      const result = await signInWithPopup(auth, provider);
+      const res = await fetch('/api/auth/google', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
+      const data = await res.json();
+      console.log(data);
+      dispatch(signInSuccess(data));
+      navigate('/');
+    } catch (error) {
+      console.log('could not login with google', error);
     }
+  };
   return (
-    <button type='button' onClick={handleGoogleClick} className='bg-red-700 text-white rounded-lg p-3 uppercase hover:opacity-95'>Continue with Google</button>
-  )
+    <button
+      type='button'
+      onClick={handleGoogleClick}
+      className='bg-red-700 text-white rounded-lg p-3 uppercase hover:opacity-95'
+    >
+      Continue with google
+    </button>
+  );
 }
